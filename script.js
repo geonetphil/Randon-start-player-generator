24095f1fb8635a5d7428f9f8b46ffe81ea9cf77c
geonetphil <philipjabry@hotmail.com>
2023-03-26 18:13:18 +0000

    removal logic more robusta

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const circles = [];
-function displayCircles(touchStartEvent) {
 
+function toClassName(identifier) {
+    return "circle-" + identifier;
+}
+
+function displayCircles(touchStartEvent) {
+    touchStartEvent.preventDefault();
     console.log(touchStartEvent);
     const touches = touchStartEvent.touches;
 
@@ -8,6 +12,8 @@ function displayCircles(touchStartEvent) {
         
         const circle = document.createElement("div")
         circle.classList.add("circle")
+        const className = toClassName(touch.identifier);
+        circle.classList.add(className)
         const height = 50;
         const radius = height / 2;
         circle.style.width = height + "px";
@@ -20,9 +26,8 @@ function displayCircles(touchStartEvent) {
     
         circle.style.top = top + "px"
         circle.style.left = left + "px"
-        document.body.appendChild(circle)
 
-        circles[touch.identifier] = circle;
+        document.body.appendChild(circle)
 
     }
     
@@ -32,17 +37,26 @@ document.body.addEventListener("touchstart", displayCircles);
 
 
 function removeCircles(touchRemoveEvent) {
-
+    touchRemoveEvent.preventDefault();
     console.log(touchRemoveEvent)
     const touches = touchRemoveEvent.changedTouches
 
     for (const touch of touches) {
-        console.log(circles);
-        const circle = circles[touch.identifier]
-        circle.remove();
+        const className = toClassName(touch.identifier);
+        const existingMatchingCircles = document.body.querySelectorAll("." + className);
+        for (const element of existingMatchingCircles) {
+            element.remove();
+        }
     }
     
 }
 
 document.body.addEventListener("touchend", removeCircles);
 document.body.addEventListener("touchcancel", removeCircles);
+
+
+function moveCircles(touchMoveEvent) {
+    touchMoveEvent.preventDefault();
+}
+
+document.body.addEventListener("touchmove", moveCircles);
